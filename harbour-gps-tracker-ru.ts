f1787a5e0041badf08a8db6168e7756768cd5fad
DashBolt <63010512+DashBolt@users.noreply.github.com>
2020-05-15 13:40:10 -0400

    Add files via upload

---
+++ harbour-gps-tracker-ru.ts
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="39"/>
+        <source>GPS Tracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImportedTracksScreen</name>
+    <message>
+        <location filename="../qml/pages/ImportedTracksScreen.qml" line="53"/>
+        <source>Imported tracks:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlaceScreen</name>
+    <message>
+        <location filename="../qml/pages/PlaceScreen.qml" line="50"/>
+        <source>Favourite places:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceScreen.qml" line="164"/>
+        <source>You don&apos;t have any point</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RecordedTracksScreen</name>
+    <message>
+        <location filename="../qml/pages/RecordedTracksScreen.qml" line="56"/>
+        <source>Recorded tracks: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RenamePlace</name>
+    <message>
+        <location filename="../qml/pages/RenamePlace.qml" line="42"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RenamePlace.qml" line="43"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RenamePlace.qml" line="47"/>
+        <source>Name place: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RenamePlace.qml" line="55"/>
+        <location filename="../qml/pages/RenamePlace.qml" line="56"/>
+        <source>Place</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SavePlace</name>
+    <message>
+        <location filename="../qml/pages/SavePlace.qml" line="43"/>
+        <source>Add point</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SavePlace.qml" line="44"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SavePlace.qml" line="54"/>
+        <source>Name place: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SavePlace.qml" line="63"/>
+        <location filename="../qml/pages/SavePlace.qml" line="64"/>
+        <source>Place</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SaveTrackScreen</name>
+    <message>
+        <location filename="../qml/pages/SaveTrackScreen.qml" line="41"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SaveTrackScreen.qml" line="42"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SaveTrackScreen.qml" line="46"/>
+        <source>Track: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SaveTrackScreen.qml" line="54"/>
+        <location filename="../qml/pages/SaveTrackScreen.qml" line="55"/>
+        <source>Track-01.05.2017.12:30</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SaveTrackScreen.qml" line="59"/>
+        <source>About track: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SaveTrackScreen.qml" line="68"/>
+        <location filename="../qml/pages/SaveTrackScreen.qml" line="69"/>
+        <source>Something about the track</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
